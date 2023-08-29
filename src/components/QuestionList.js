55102e0501f096d4e8ec5bfdeb06ff8d4fe5a67c
Victor <wanjala94vic@gmail.com>
2023-08-29 22:51:28 +0300

    displaying questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,39 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch("http://localhost:4000/questions");
+        const data = await response.json();
+        setQuestions(data);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            <div>
+              <p>{question.prompt}</p>
+              <p>Answers: {question.answers.join(", ")}</p>
+              <p>Correct Index: {question.correctIndex}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
 
 export default QuestionList;
+
