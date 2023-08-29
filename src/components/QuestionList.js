279d13dc87bbf01f5462257e3699e74736d097c2
Victor <wanjala94vic@gmail.com>
2023-08-29 23:13:54 +0300

    completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
   const [questions, setQuestions] = useState([]);
@@ -17,6 +17,48 @@ function QuestionList() {
     fetchQuestions();
   }, []);
 
+  async function deleteQuestion(id) {
+    try {
+      const response = await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE"
+      });
+
+      if (response.ok) {
+        setQuestions(questions.filter(question => question.id !== id));
+        console.log("Question deleted successfully!");
+      } else {
+        console.error("Failed to delete question");
+      }
+    } catch (error) {
+      console.error("Error deleting question:", error);
+    }
+  }
+
+  async function updateCorrectAnswer(id, correctIndex) {
+    try {
+      const response = await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ correctIndex: parseInt(correctIndex) })
+      });
+
+      if (response.ok) {
+        setQuestions(prevQuestions =>
+          prevQuestions.map(question =>
+            question.id === id ? { ...question, correctIndex: parseInt(correctIndex) } : question
+          )
+        );
+        console.log("Correct answer updated successfully!");
+      } else {
+        console.error("Failed to update correct answer");
+      }
+    } catch (error) {
+      console.error("Error updating correct answer:", error);
+    }
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
@@ -27,6 +69,17 @@ function QuestionList() {
               <p>{question.prompt}</p>
               <p>Answers: {question.answers.join(", ")}</p>
               <p>Correct Index: {question.correctIndex}</p>
+              <select
+                value={question.correctIndex}
+                onChange={(event) => updateCorrectAnswer(question.id, event.target.value)}
+              >
+                {question.answers.map((answer, index) => (
+                  <option key={index} value={index}>
+                    {answer}
+                  </option>
+                ))}
+              </select>
+              <button onClick={() => deleteQuestion(question.id)}>Delete</button>
             </div>
           </li>
         ))}
@@ -36,4 +89,5 @@ function QuestionList() {
 }
 
 export default QuestionList;
+
 
